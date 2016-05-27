78562b206dd5d42f47c20035f5292aa1f3c505b2
James Sturtevant <jsturtevant@gmail.com>
2016-05-27 15:36:35 -0400

    trying to get anything to work

--- install_hoodie.sh
+++ install_hoodie.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
-docker run -d --name my-couchdb \
-    -v /data/couchdb:/usr/local/var/lib/couchdb \
-    klaemo/couchdb:1.6.1
-# create a admin user with the password `secret`
-# of course you can use you own username/password
-docker run -it --rm \
-    --link my-couchdb:couchdb \
-    yxdhde/alpine-curl-git curl -X PUT \
-    couchdb:5984/_config/admins/admin -d '"secret"'
-# login with the admin user
-docker run -it --rm \
-    --link my-couchdb:couchdb \
-    yxdhde/alpine-curl-git curl -X POST \
-    -H 'Content-Type: application/x-www-form-urlencoded' \
-    couchdb:5984/_session -d 'name=admin&password=secret'
+# write some HTML
+echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/demo.html
 
-docker run -d -p 8080:8080 \
-    --name my-app \
-    --link my-couchdb:couchdb \
-    -e hoodie_dbUrl=http://admin:secret@couchdb:5984/ \
-    hoodiehq/hoodie-app-tracker

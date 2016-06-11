393a9b432f760cd30a36c38f9b3177e3bb2c4ffd
James Sturtevant <jsturtevant@gmail.com>
2016-06-11 16:51:40 -0400

    pass password into the script

--- install_hoodie.sh
+++ install_hoodie.sh
@@ -2,21 +2,21 @@
 docker run -d --name my-couchdb \
     -v /data/couchdb:/usr/local/var/lib/couchdb \
     klaemo/couchdb:1.6.1
-# create a admin user with the password `secret`
+# create a admin user with the password 
 # of course you can use you own username/password
+pass=$1
 docker run --rm \
     --link my-couchdb:couchdb \
     yxdhde/alpine-curl-git curl -X PUT \
-    couchdb:5984/_config/admins/admin -d '"$1"'
+    couchdb:5984/_config/admins/admin -d '"'$pass'"'
 # login with the admin user
 docker run --rm \
     --link my-couchdb:couchdb \
     yxdhde/alpine-curl-git curl -X POST \
     -H 'Content-Type: application/x-www-form-urlencoded' \
-    couchdb:5984/_session -d 'name=admin&password=$1'
-
+    couchdb:5984/_session -d 'name=admin&password='$pass
 docker run -d -p 8080:8080 \
     --name my-app \
     --link my-couchdb:couchdb \
-    -e hoodie_dbUrl=http://admin:$1@couchdb:5984/ \
+    -e hoodie_dbUrl=http://admin:$pass@couchdb:5984/ \
     hoodiehq/hoodie-app-tracker
